51bc53a6e664c64e4592f30faf598937b4d7713e
NowayXD <95932961+NowayXD@users.noreply.github.com>
2022-04-11 22:27:21 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/NowayXD/Movies-Master /EvaMaria
+  git clone https://github.com/NowayXD/Movies-Master.git /EvaMaria
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /EvaMaria
