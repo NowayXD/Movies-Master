f7facfffda04f929e5f114a4aeee6637c8ccf738
NowayXD <95932961+NowayXD@users.noreply.github.com>
2022-04-12 05:10:15 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/NowayXD/Movies-Master /Movies-Master
+  git clone https://github.com/EvamariaTG/EvaMaria.git /EvaMaria
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Movies-Master
+  git clone $UPSTREAM_REPO /EvaMaria
 fi
-cd /Movies-Master
+cd /EvaMaria
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
